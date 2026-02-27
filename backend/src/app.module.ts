f4f4f0b075201b7a9d3f0e2b0cb98d49403672a4
CastroFabio <fmpc95@gmail.com>
2026-02-26 23:37:21 -0300

    crud respondendo: customer

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CustomerModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna o ConfigModule global
+      envFilePath: '.env', // Caminho para o arquivo .env
+    }),
+    PrismaModule,
+    CustomerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
