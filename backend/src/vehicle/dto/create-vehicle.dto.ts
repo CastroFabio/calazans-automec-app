90d01e1d1bce82e8a8d45a9cdf1779c57b13ef17
CastroFabio <fmpc95@gmail.com>
2026-08-03 18:09:25 -0300

    começando vehicle

--- backend/src/vehicle/dto/create-vehicle.dto.ts
+++ backend/src/vehicle/dto/create-vehicle.dto.ts
@@ -1,1 +1,34 @@
-export class CreateVehicleDto {}
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsPhoneNumber,
+} from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class CreateVehicleDto {
+  @ApiProperty({
+    description: 'Placa do veículo',
+    example: 'ABC-1234',
+    minLength: 8,
+  })
+  @IsNotEmpty({ message: 'Placa é obrigatória' })
+  @IsString({ message: 'Placa deve ser uma string' })
+  license_plate: string;
+
+  @ApiProperty({
+    description: 'Marca do veículo',
+    example: 'Toyota',
+  })
+  @IsString({ message: 'Marca deve ser uma string' })
+  @IsOptional()
+  brand: string;
+
+  @ApiProperty({
+    description: 'Modelo do veículo',
+    example: 'Corolla',
+  })
+  @IsString({ message: 'Modelo deve ser uma string' })
+  @IsOptional()
+  model: string;
+}
